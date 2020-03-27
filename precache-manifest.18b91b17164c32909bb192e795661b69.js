030787a8af589af830cc5baf75bb81b7ebb18cc7
Paolo Navarretta <paolo@Paolos-MacBook-Pro.local>
2020-03-27 14:28:04 +0100

    Updates

---
+++ precache-manifest.18b91b17164c32909bb192e795661b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c457fb3d9e870de0cf3c1a88767979d5",
+    "url": "/react-google-books-search/index.html"
+  },
+  {
+    "revision": "0bad09e02a87a50e98a2",
+    "url": "/react-google-books-search/static/css/main.c58c4443.chunk.css"
+  },
+  {
+    "revision": "5ffab6229f6cd496819e",
+    "url": "/react-google-books-search/static/js/2.4b29a56a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-google-books-search/static/js/2.4b29a56a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bad09e02a87a50e98a2",
+    "url": "/react-google-books-search/static/js/main.90e4542b.chunk.js"
+  },
+  {
+    "revision": "6bf6fe84e16f06228ec4",
+    "url": "/react-google-books-search/static/js/runtime-main.6bf1dab2.js"
+  }
+]);
